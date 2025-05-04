7fe14d53a3bdcf0c1d28c657d9eb5f73403cfacd
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2025-05-04 17:26:19 +0200

    animationmanager update: can play multiple animationPlayers

--- scripts/AnimationManager.gd
+++ scripts/AnimationManager.gd
@@ -1,8 +1,13 @@
 extends Node2D
 class_name AnimationManager
 
-@export var sprite: Sprite2D
-@export var animation_player: AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+var animation_players: Array = []
+
+func _ready() -> void:
+	for child in get_children():
+		if child is AnimationPlayer:
+			animation_players.append(child)
 
 func play(animation_name: String, reverse: bool = false) -> void:
 	animation_name.to_lower()
@@ -12,11 +17,12 @@ func play(animation_name: String, reverse: bool = false) -> void:
 func play_animation_player(animation_name: String, reverse: bool = false) -> void:
 	animation_name.to_lower()
 	
-	if animation_player and animation_player.has_animation(animation_name):
-		if reverse:
-			animation_player.play_backwards(animation_name)
-		else:
-			animation_player.play(animation_name)
+	for animation_player in animation_players:
+		if animation_player and animation_player.has_animation(animation_name):
+			if reverse:
+				animation_player.play_backwards(animation_name)
+			else:
+				animation_player.play(animation_name)
 
 func flip_sprite(flip_h: bool = true, flip_v: bool = true) -> void:
 	if sprite:
