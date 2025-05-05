58b1a44703a98f8f9a0df3ff3768ce224e0a1ec4
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2025-05-05 23:08:27 +0200

    files clean

--- scripts/global_components/stamina_component.gd
+++ scripts/global_components/stamina_component.gd
@@ -14,20 +14,14 @@ var low_stamina_threshold: float:
 var stamina := max_stamina
 var has_passed_threshold := false
 
-func _ready() -> void:
-	print("max_stamina: ", max_stamina)
-	print("stamina_threshold: ", low_stamina_threshold)
-
 func drain_stamina(drain_value := stamina_drain_value) -> void:
 	stamina -= drain_value
 	if stamina <= low_stamina_threshold && !has_passed_threshold:
 		anim_tree.get("parameters/playback").travel("LowStamina")
-		print("quoi")
 		has_passed_threshold = true
 
 func refill_stamina() -> void:
 	if stamina <= low_stamina_threshold && has_passed_threshold:
 		anim_tree.get("parameters/playback").travel("RESET")
-		print("coubeh")
 		has_passed_threshold = false
 	stamina = max_stamina
