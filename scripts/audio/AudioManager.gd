2b399da5d9eabed404b4edb387f7168c92c02435
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2025-05-07 16:04:25 +0200

    audio manager plays all of types

--- scripts/audio/AudioManager.gd
+++ scripts/audio/AudioManager.gd
@@ -20,32 +20,34 @@ func _ready() -> void:
 		playing_sounds[sound_effect.type] = []  # Initialize an empty list for each sound type
 
 
-## Creates a sound effect at a specific location, stopping the oldest sound if the limit is reached.
+## Plays all sounds of a specific type at a given location, stopping the oldest sound if the limit is reached.
 func create_2d_audio_at_location_with_culling(location: Vector2, type: SoundEffect.SOUND_EFFECT_TYPE) -> void:
 	if sound_effect_dict.has(type):
 		var sound_effect: SoundEffect = sound_effect_dict[type]
 		
 		# Check if the limit is reached
-		if playing_sounds[type].size() >= sound_effect.limit:
+		while playing_sounds[type].size() >= sound_effect.limit:
 			# Stop the oldest sound
 			var oldest_audio = playing_sounds[type].pop_front()
 			if oldest_audio:
 				oldest_audio.stop()
 				oldest_audio.queue_free()
 		
-		# Create and play the new sound
-		var new_2D_audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
-		add_child(new_2D_audio)
-		new_2D_audio.position = location
-		new_2D_audio.stream = sound_effect.sound_effect
-		new_2D_audio.volume_db = sound_effect.volume
-		new_2D_audio.pitch_scale = sound_effect.pitch_scale
-		new_2D_audio.pitch_scale += randf_range(-sound_effect.pitch_randomness, sound_effect.pitch_randomness)
-		new_2D_audio.finished.connect(self._on_audio_finished.bind(new_2D_audio, type))
-		new_2D_audio.play()
-		
-		# Add the new sound to the tracking list
-		playing_sounds[type].append(new_2D_audio)
+		# Play all sounds of this type
+		for sound in sound_effects:
+			if sound.type == type:
+				var new_2D_audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+				add_child(new_2D_audio)
+				new_2D_audio.position = location
+				new_2D_audio.stream = sound.sound_effect
+				new_2D_audio.volume_db = sound.volume
+				new_2D_audio.pitch_scale = sound.pitch_scale
+				new_2D_audio.pitch_scale += randf_range(-sound.pitch_randomness, sound.pitch_randomness)
+				new_2D_audio.finished.connect(self._on_audio_finished.bind(new_2D_audio, type))
+				new_2D_audio.play()
+				
+				# Add the new sound to the tracking list
+				playing_sounds[type].append(new_2D_audio)
 	else:
 		push_error("Audio Manager failed to find setting for type ", type)
 
@@ -60,42 +62,44 @@ func _on_audio_finished(audio: AudioStreamPlayer2D, type: SoundEffect.SOUND_EFFE
 ## Creates a sound effect at a specific location, with optional looping.
 func create_2d_audio_at_location(location: Vector2, type: SoundEffect.SOUND_EFFECT_TYPE, loop: bool = false) -> void:
 	if sound_effect_dict.has(type):
-		var sound_effect: SoundEffect = sound_effect_dict[type]
-		if sound_effect.has_open_limit():
-			sound_effect.change_audio_count(1)
-			var new_2D_audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
-			add_child(new_2D_audio)
-			new_2D_audio.position = location
-			new_2D_audio.stream = sound_effect.sound_effect
-			new_2D_audio.volume_db = sound_effect.volume
-			new_2D_audio.pitch_scale = sound_effect.pitch_scale
-			new_2D_audio.pitch_scale += randf_range(-sound_effect.pitch_randomness, sound_effect.pitch_randomness)
-			if loop:
-				new_2D_audio.finished.connect(_on_audio_loop.bind(new_2D_audio))
-			else:
-				new_2D_audio.finished.connect(sound_effect.on_audio_finished)
-				new_2D_audio.finished.connect(new_2D_audio.queue_free)
-			
-			new_2D_audio.play()
+		for sound in sound_effects:
+			if sound.type == type:
+				if sound.has_open_limit():
+					sound.change_audio_count(1)
+					var new_2D_audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+					add_child(new_2D_audio)
+					new_2D_audio.position = location
+					new_2D_audio.stream = sound.sound_effect
+					new_2D_audio.volume_db = sound.volume
+					new_2D_audio.pitch_scale = sound.pitch_scale
+					new_2D_audio.pitch_scale += randf_range(-sound.pitch_randomness, sound.pitch_randomness)
+					if loop:
+						new_2D_audio.finished.connect(_on_audio_loop.bind(new_2D_audio))
+					else:
+						new_2D_audio.finished.connect(sound.on_audio_finished)
+						new_2D_audio.finished.connect(new_2D_audio.queue_free)
+					
+					new_2D_audio.play()
 	else:
 		push_error("Audio Manager failed to find setting for type ", type)
 
 
-## Creates a sound effect, with optional looping.
+## Creates all sound effects of a specific type, with optional looping.
 func create_audio(type: SoundEffect.SOUND_EFFECT_TYPE) -> void:
 	if sound_effect_dict.has(type):
-		var sound_effect: SoundEffect = sound_effect_dict[type]
-		if sound_effect.has_open_limit():
-			sound_effect.change_audio_count(1)
-			var new_audio: AudioStreamPlayer = AudioStreamPlayer.new()
-			add_child(new_audio)
-			new_audio.stream = sound_effect.sound_effect
-			new_audio.volume_db = sound_effect.volume
-			new_audio.pitch_scale = sound_effect.pitch_scale
-			new_audio.pitch_scale += randf_range(-sound_effect.pitch_randomness, sound_effect.pitch_randomness)
-			new_audio.finished.connect(sound_effect.on_audio_finished)
-			new_audio.finished.connect(new_audio.queue_free)
-			new_audio.play()
+		for sound in sound_effects:
+			if sound.type == type:
+				if sound.has_open_limit():
+					sound.change_audio_count(1)
+					var new_audio: AudioStreamPlayer = AudioStreamPlayer.new()
+					add_child(new_audio)
+					new_audio.stream = sound.sound_effect
+					new_audio.volume_db = sound.volume
+					new_audio.pitch_scale = sound.pitch_scale
+					new_audio.pitch_scale += randf_range(-sound.pitch_randomness, sound.pitch_randomness)
+					new_audio.finished.connect(sound.on_audio_finished)
+					new_audio.finished.connect(new_audio.queue_free)
+					new_audio.play()
 	else:
 		push_error("Audio Manager failed to find setting for type ", type)
 
