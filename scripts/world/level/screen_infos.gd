c4298e595d23d57e242a9213367278d9f5f330fd
clement.mondary <clement.mondary@edu.ecole-89.com>
2025-05-15 14:52:33 +0200

    screen_0_0 progression...

---
+++ scripts/world/level/screen_infos.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name ScreenInfos
+
+# Just copy-past the exemple camera settings
+@export var camera_zoom: float = 2.0
+@export var local_camera_limit_top: int = 0.0
+@export var local_camera_limit_right: int = 0.0
