9ae53ccc5f0d558e9997e801d1584d8dd1913f60
Nemeco_ <clement.mondary@edu.ecole-89.com>
2025-08-26 01:08:40 +0200

    swapped min theme for: Bounded by Thoughts

--- scripts/player/hud/diamond_tracker.gd
+++ scripts/player/hud/diamond_tracker.gd
@@ -3,14 +3,14 @@ extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var diamond_tracker_label: Label = $DiamondTrackerLabel
 
-var diamonds := []
+var lootable_diamonds := []
 var is_hud_visible := false
 
 func _ready() -> void:
+	visible = false
 	_update_tracker()
-	diamonds = get_tree().get_nodes_in_group("diamond")
-	for diamond in diamonds:
-		print("connecting diamond...")
+	lootable_diamonds = get_tree().get_nodes_in_group("diamond")
+	for diamond in lootable_diamonds:
 		diamond.diamond_collected.connect(_diamond_looted)
 
 func _diamond_looted() -> void:
@@ -19,11 +19,11 @@ func _diamond_looted() -> void:
 			animation_player.stop()
 			animation_player.play("update")
 		false:
+			visible = true
 			animation_player.play("fade_out")
 
 func _update_tracker() -> void:
 	diamond_tracker_label.text = "x " + str(DiamondManager.get_total_collected())
-	print(DiamondManager.collected_items)
 
 func _play_update_sfx() -> void:
 	AudioManager.create_audio(SoundEffect.SOUND_EFFECT_TYPE.ON_DIAMOND_TRACKER_UPDATED)
@@ -33,6 +33,8 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		"fade_out":
 			is_hud_visible = true
 			animation_player.play("update")
+		"fade_in":
+			visible = false
 		"update":
 			is_hud_visible = false
 			animation_player.play("fade_in")
